220e98b52228d1253abbc45dd5d913314e83177d
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-01-30 11:17:53 +0800

    Add product model spec: create new product with user association

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:product) { FactoryBot.create(:product) }
+
+  context 'create new product' do
+    it 'able to create new product' do
+      product.save
+      expect(Product.all.count).to eq(1)
+    end
+
+    it 'able to create new product with association with user' do
+      product.save
+      expect(product.user).to eq(User.find(product.user_id))
+    end
+
+    it 'able to use activate! method' do
+      product.activate!
+      expect(product.is_active).to eq(true)
+    end
+  end
 end
