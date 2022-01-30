6a4fd5d9fdeb8eb01b45e0a0f3f260f777386b5a
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-01-31 03:03:15 +0800

    Include user into products index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
 
   # GET /products
   def index
-    @products = Product.all
+    @products = Product.all.includes(:user)
 
     render json: @products
   end
